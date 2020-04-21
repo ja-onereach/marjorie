c4340c252928ffc9b10e8692668d162adb5ac487
Jonathan Anderson <jonathan@nothingtrue.com>
2020-04-21 09:47:09 -0600

    update main dialog again

--- dialogs/mainDialog.js
+++ dialogs/mainDialog.js
@@ -76,49 +76,49 @@ class MainDialog extends ComponentDialog {
         const doBypass = await or.bypass(stepContext.context._activity, 'actStep');
         console.log('doBypass returned', doBypass);
         // if (doBypass) return await stepContext.endDialog();
+        if (!doBypass) {
+            const bookingDetails = {};
 
-        const bookingDetails = {};
+            if (!this.luisRecognizer.isConfigured) {
+                // LUIS is not configured, we just run the BookingDialog path.
+                return await stepContext.beginDialog('bookingDialog', bookingDetails);
+            }
 
-        if (!this.luisRecognizer.isConfigured) {
-            // LUIS is not configured, we just run the BookingDialog path.
-            return await stepContext.beginDialog('bookingDialog', bookingDetails);
+            // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt)
+            const luisResult = await this.luisRecognizer.executeLuisQuery(stepContext.context);
+            switch (LuisRecognizer.topIntent(luisResult)) {
+            case 'BookFlight': {
+                // Extract the values for the composite entities from the LUIS result.
+                const fromEntities = this.luisRecognizer.getFromEntities(luisResult);
+                const toEntities = this.luisRecognizer.getToEntities(luisResult);
+
+                // Show a warning for Origin and Destination if we can't resolve them.
+                await this.showWarningForUnsupportedCities(stepContext.context, fromEntities, toEntities);
+
+                // Initialize BookingDetails with any entities we may have found in the response.
+                bookingDetails.destination = toEntities.airport;
+                bookingDetails.origin = fromEntities.airport;
+                bookingDetails.travelDate = this.luisRecognizer.getTravelDate(luisResult);
+                console.log('LUIS extracted these booking details:', JSON.stringify(bookingDetails));
+
+                // Run the BookingDialog passing in whatever details we have from the LUIS call, it will fill out the remainder.
+                return await stepContext.beginDialog('bookingDialog', bookingDetails);
+            }
+
+            case 'GetWeather': {
+                // We haven't implemented the GetWeatherDialog so we just display a TODO message.
+                const getWeatherMessageText = 'TODO: get weather flow here';
+                await stepContext.context.sendActivity(getWeatherMessageText, getWeatherMessageText, InputHints.IgnoringInput);
+                break;
+            }
+
+            default: {
+                // Catch all for unhandled intents
+                const didntUnderstandMessageText = `Sorry, I didn't get that. Please try asking in a different way (intent was ${ LuisRecognizer.topIntent(luisResult) })`;
+                await stepContext.context.sendActivity(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
+            }
+            }
         }
-
-        // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt)
-        const luisResult = await this.luisRecognizer.executeLuisQuery(stepContext.context);
-        switch (LuisRecognizer.topIntent(luisResult)) {
-        case 'BookFlight': {
-            // Extract the values for the composite entities from the LUIS result.
-            const fromEntities = this.luisRecognizer.getFromEntities(luisResult);
-            const toEntities = this.luisRecognizer.getToEntities(luisResult);
-
-            // Show a warning for Origin and Destination if we can't resolve them.
-            await this.showWarningForUnsupportedCities(stepContext.context, fromEntities, toEntities);
-
-            // Initialize BookingDetails with any entities we may have found in the response.
-            bookingDetails.destination = toEntities.airport;
-            bookingDetails.origin = fromEntities.airport;
-            bookingDetails.travelDate = this.luisRecognizer.getTravelDate(luisResult);
-            console.log('LUIS extracted these booking details:', JSON.stringify(bookingDetails));
-
-            // Run the BookingDialog passing in whatever details we have from the LUIS call, it will fill out the remainder.
-            return await stepContext.beginDialog('bookingDialog', bookingDetails);
-        }
-
-        case 'GetWeather': {
-            // We haven't implemented the GetWeatherDialog so we just display a TODO message.
-            const getWeatherMessageText = 'TODO: get weather flow here';
-            await stepContext.context.sendActivity(getWeatherMessageText, getWeatherMessageText, InputHints.IgnoringInput);
-            break;
-        }
-
-        default: {
-            // Catch all for unhandled intents
-            const didntUnderstandMessageText = `Sorry, I didn't get that. Please try asking in a different way (intent was ${ LuisRecognizer.topIntent(luisResult) })`;
-            await stepContext.context.sendActivity(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
-        }
-        }
-
         return await stepContext.next();
     }
 
